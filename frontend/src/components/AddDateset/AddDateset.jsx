2c7a591afb6a55d06f3f35a112a94ac656499461
Turkunov Y <55660526+turkunov@users.noreply.github.com>
2023-09-22 17:17:38 +0300

    updated frontend

--- frontend/src/components/AddDateset/AddDateset.jsx
+++ frontend/src/components/AddDateset/AddDateset.jsx
@@ -1,20 +1,47 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import style from "./AddDateset.module.css"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import style from "./AddDateset.module.css";
 
 const AddDateset = () => {
+    // Создаем состояние для хранения выбранного файла
+    const [selectedFile, setSelectedFile] = useState(null);
 
-    const [selectedFile, setSelectedFile] = useState(null)
+    // Функция для обработки загрузки файла
+    const handleFileUpload = async () => {
+        // Проверяем, был ли выбран файл
+        if (selectedFile) {
+            // Создаем объект FormData для отправки файла на сервер
+            const formData = new FormData();
+            formData.append('dataset', selectedFile); // Добавляем файл в FormData
 
-    const handleFileUpload = event => {
-        const file = event.target.files[0];
-        setSelectedFile(file);
+            try {
+                // Отправляем POST запрос на сервер с использованием FormData
+                const response = await fetch('https://antares-production.up.railway.app/api/dataset_inference', {
+                    method: 'POST', // Используем метод POST
+                    body: formData, // Передаем FormData с файлом
+                });
+
+                // Проверяем, был ли успешный ответ от сервера
+                if (!response.ok) {
+                    throw new Error('Не удалось загрузить файл на сервер');
+                }
+
+                // Ваш код для обработки успешной загрузки файла
+                console.log('Файл успешно загружен на сервер');
+            } catch (error) {
+                // Обработка ошибок, если загрузка не удалась
+                console.error(error);
+            }
+        } else {
+            // Обработка случая, когда файл не выбран
+            console.warn('Выберите файл для загрузки');
+        }
     };
 
     return (
         <div className={style.main}>
             <label className={style.window}>
-                <input type="file" onChange={handleFileUpload} accept=".xlsx" />
+                <input type="file" onChange={event => setSelectedFile(event.target.files[0])} accept=".xlsx" />
                 {selectedFile ? (
                     <span>Вы выбрали файл: {selectedFile.name}</span>
                 ) : (
@@ -22,13 +49,13 @@ const AddDateset = () => {
                 )}
             </label>
             <div className={style.btns}>
-                <button >Сохранить</button>
+                <button onClick={handleFileUpload}>Сохранить</button>
                 <Link to="/AiNews/">
                     <button>Отменить</button>
                 </Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AddDateset
+export default AddDateset;
--- frontend/src/components/AddNews/AddNews.jsx
+++ frontend/src/components/AddNews/AddNews.jsx
@@ -1,27 +1,65 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import style from "./AddNews.module.css"
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { Link } from 'react-router-dom';
+import style from "./AddNews.module.css";
 
 const AddNews = () => {
+    // Создаем состояния для хранения заголовка, описания и классификации новости
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [classification, setClassification] = useState(null);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("")
-
-    const handleClassAndDuplicates = () => {
-        // Запрос на сервер для загрузки данных в базу данных
-        fetch('ссылка_на_API', {
-            method: 'POST',
-            body: JSON.stringify({ title, description })
-        })
-            .then(response => response.json())
-            .then(data => {
-                const { removed, classes } = data;
-                console.log('Removed duplicates:', removed);
-                console.log('Classes:', classes);
-                // Здесь можно обработать полученные данные
-            });
+    // Стили для модального окна
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            maxWidth: '80%',
+            padding: '20px',
+        },
     };
 
+    // Функция для отправки запроса на API и определения класса новости
+    const handleClassAndDuplicates = async () => {
+        try {
+            // Создаем URL для запроса, добавляя параметры title и description
+            const url = `https://antares-production.up.railway.app/api/single_inference/?channelid=${encodeURIComponent(title)}&text=${encodeURIComponent(description)}`;
+
+            // Отправляем GET запрос на указанный URL
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            // Проверяем, был ли успешный ответ от сервера
+            if (!response.ok) {
+                throw new Error('Не смог получить интерфейс');
+            }
+
+            // Получаем ответ от сервера в формате JSON
+            const inference = await response.json();
+
+            // Устанавливаем результат классификации в состояние
+            setClassification(inference.classification);
+
+            // Выводим результат классификации в консоль
+            console.log('Класс новости:', inference.classification);
+        } catch (error) {
+            // Обрабатываем ошибку, если запрос не удался
+            console.error(error);
+        }
+    }
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    }
 
     return (
         <div className={style.news}>
@@ -29,13 +67,24 @@ const AddNews = () => {
             <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Введите заголовок' />
             <textarea onChange={(e) => setDescription(e.target.value)} value={description} placeholder='Введите описание'></textarea>
             <div className={style.btns}>
+                {/* Вызываем функцию handleClassAndDuplicates при клике на кнопке */}
                 <button onClick={handleClassAndDuplicates}>Определить класс и дубликаты</button>
                 <Link to="/AiNews/">
                     <button>Отменить</button>
                 </Link>
             </div>
+            {/* Отображаем результат классификации, если он есть */}<Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Результат классификации"
+            >
+                <h2>Результат классификации</h2>
+                {classification && <p>Класс новости: {classification}</p>}
+                <button onClick={closeModal}>Закрыть</button>
+            </Modal>
         </div>
-    )
+    );
 }
 
-export default AddNews
+export default AddNews;
